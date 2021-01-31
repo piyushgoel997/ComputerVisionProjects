66a86e446b6516cb05d7c7d6624ddfd8aa84c0f4
piyush <piyushgoel997@gmail.com>
2021-01-31 04:33:38 -0500

    finish task 6 - sobel gradient

--- filter.cpp
+++ filter.cpp
@@ -117,6 +117,19 @@ int sobolY3x3(cv::Mat& src, cv::Mat& dst) {
 	return 0;
 }
 
+int sobol3x3Gradient(cv::Mat& src, cv::Mat& dst) {
+	cv::Mat x(src.rows, src.cols, CV_16SC3);
+	cv::Mat y(src.rows, src.cols, CV_16SC3);
+	sobolX3x3(src, x);
+	sobolY3x3(src, y);
+	x.convertTo(x, CV_64FC3);
+	y.convertTo(y, CV_64FC3);
+	cv::Mat prod = x.mul(x) + y.mul(y);
+	cv::pow(prod, 0.5, dst);
+	dst.convertTo(dst, CV_8UC3);
+	return 0;
+}
+
 
 int main() {
 
@@ -134,15 +147,15 @@ int main() {
 	cv::Mat modified(img.rows, img.cols, CV_16SC3);
 
 	// blur5x5(img, blurred);
-	sobolY3x3(img, modified);
+	sobol3x3Gradient(img, modified);
 	cv::abs(modified);
 	modified.convertTo(modified, CV_8UC3);
 
-	cv::imshow("blurred", modified);
+	cv::imshow("modified", modified);
 
 	while (true) {
 		auto k = cv::waitKey(0);
 		if (k == 'q') { return 0; }
-		if (k == 's') { cv::imwrite("sobely.jpg", modified); }
+		if (k == 's') { cv::imwrite("sobelgrad.jpg", modified); }
 	}
 }
--- filter.h
+++ filter.h
@@ -6,3 +6,5 @@ int blur5x5(cv::Mat& src, cv::Mat& dst);
 int sobolX3x3(cv::Mat& src, cv::Mat& dst);
 
 int sobolY3x3(cv::Mat& src, cv::Mat& dst);
+
+int sobol3x3Gradient(cv::Mat& src, cv::Mat& dst);
