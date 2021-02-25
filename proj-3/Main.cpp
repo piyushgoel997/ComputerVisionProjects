1319c5ae03116f676d41ea7ab6613fcc6e0c5df4
piyush <piyushgoel997@gmail.com>
2021-02-25 12:40:10 -0500

    implement segment and color function

--- proj-3/Main.cpp
+++ proj-3/Main.cpp
@@ -25,13 +25,16 @@ int main() {
 
 		// threshold the video
 		// TODO do thresholding with the histogram and k-means
-		cv::Mat temp(frame.rows, frame.cols, CV_8UC3);
-		threshold<cv::Vec3b>(frame, temp, 200.0);
-				
+		cv::Mat thresh(frame.size(), CV_8UC1);
+		threshold<cv::Vec3b>(frame, thresh, 200.0);
+
+		cv::Mat segmented(frame.size(), CV_8UC3);
+		segmentAndColorRegions(thresh, segmented);
 
 		cv::imshow("original_video", frame);
-		cv::imshow("thresholded_video", temp);
-		auto k = cv::waitKey(10); // why does setting this to zero doesn't work?
+		cv::imshow("thresholded_video", thresh);
+		cv::imshow("segmented_video", segmented);
+		auto k = cv::waitKey(10);
 		if (k == 'q') { break; }
 		if (k == 's') {
 			cv::imwrite("screencapture_" + std::to_string(scNum) + ".jpg", frame);
--- proj-3/Tasks.h
+++ proj-3/Tasks.h
@@ -1,21 +1,40 @@
 #pragma once
 #include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
 
 /**
  * @brief thresholds (by making sure all color values are greater than the threshold) src and returns the result in dst.
  * @tparam T has to be of the type cv::Vec with size 3
- * @param src the matrix to be threshold-ed
- * @param dst the matrix which will contain the result
- * @param thresh the value at which to threshold
+ * @param src the matrix to be threshold-ed (the matrix is of the type T)
+ * @param dst the matrix which will contain the result (this matrix should always be of the type uchar)
+ * @param t_up upper threshold
+ * @param t_low lower threshold
 */
-template<typename T>
-void threshold(cv::Mat& src, cv::Mat& dst, const double thresh) {
+template <typename T>
+void threshold(cv::Mat& src, cv::Mat& dst, const double t_up, const double t_low = 0) {
 	for (auto i = 0; i < src.rows; ++i) {
 		for (auto j = 0; j < src.cols; ++j) {
-			if (src.at<T>(i, j)[0] > thresh && src.at<T>(i, j)[1] > thresh && src.at<T>(i, j)[2] > thresh) {
-				dst.at<cv::Vec3b>(i, j) = cv::Vec3b(255, 255, 255);
+			if ((src.at<T>(i, j)[0] > t_up && src.at<T>(i, j)[1] > t_up && src.at<T>(i, j)[2] > t_up) ||
+				(src.at<T>(i, j)[0] < t_low && src.at<T>(i, j)[1] < t_low && src.at<T>(i, j)[2] < t_low)) {
+				dst.at<uchar>(i, j) = 255;
 			}
-			else { dst.at<cv::Vec3b>(i, j) = cv::Vec3b(0, 0, 0); }
+			else { dst.at<uchar>(i, j) = 0; }
+		}
+	}
+}
+
+// ref: https://docs.opencv.org/3.4/de/d01/samples_2cpp_2connected_components_8cpp-example.html
+void segmentAndColorRegions(cv::Mat& src, cv::Mat& dst, int connectivity = 4) {
+	cv::Mat labels(src.size(), CV_32S);
+	// TODO implement own connected components algorithm
+	int numLabels = cv::connectedComponents(src, labels);
+	std::vector<cv::Vec3b> colors(numLabels);
+	colors[0] = cv::Vec3b(0, 0, 0); // black for the bkg
+	for (int i = 1; i < numLabels; ++i) { colors[i] = cv::Vec3b((rand() & 255), (rand() & 255), (rand() & 255)); }
+
+	for (int i = 0; i < src.rows; ++i) {
+		for (int j = 0; j < src.cols; ++j) {
+			dst.at<cv::Vec3b>(i,j) = colors[labels.at<int>(i, j)];
 		}
 	}
 }
