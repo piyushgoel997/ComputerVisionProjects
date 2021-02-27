89b4622aaf46ae540fe1c3d855256973bcfd042f
piyush <piyushgoel997@gmail.com>
2021-02-27 14:33:12 -0500

    refactor features

--- proj-3/Features.h
+++ proj-3/Features.h
@@ -1,13 +1,14 @@
+#pragma once
+
 #include <vector>
-#include <opencv2/core/mat.hpp>
-#define _USE_MATH_DEFINES
-#include <math.h>
+#include <algorithm>
+#define M_PI 3.14159265358979323846
 
 // TODO what should f(x,y) be?
 
 
 // define a generic moment function
-double moment(std::vector<std::pair<int, int>>& vector, int p, int q) {
+static double moment(std::vector<std::pair<int, int>>& vector, int p, int q) {
 	auto m = 0.0;
 	for (auto coord : vector) {
 		auto x = coord.first;
@@ -18,13 +19,13 @@ double moment(std::vector<std::pair<int, int>>& vector, int p, int q) {
 }
 
 // centroid function
-std::pair<double, double> findCentroid(std::vector<std::pair<int, int>>& vector, double m00 = -1) {
+static std::pair<double, double> findCentroid(std::vector<std::pair<int, int>>& vector, double m00 = -1) {
 	if (m00 == -1) { m00 = moment(vector, 0, 0); }
 	return std::pair<double, double>(moment(vector, 1, 0) / m00, moment(vector, 0, 1) / m00);
 }
 
 // a general central moment
-double centralMoment(std::vector<std::pair<int, int>>& vector, int p, int q, double m00 = -1) {
+static double centralMoment(std::vector<std::pair<int, int>>& vector, int p, int q, double m00 = -1) {
 	auto m = 0.0;
 	auto c = findCentroid(vector);
 	auto c_x = c.first;
@@ -39,13 +40,13 @@ double centralMoment(std::vector<std::pair<int, int>>& vector, int p, int q, dou
 }
 
 // orientation of the central axis
-double findAlpha(std::vector<std::pair<int, int>>& vector) {
+static double findAlpha(std::vector<std::pair<int, int>>& vector) {
 	return 0.5 * atan2(2 * centralMoment(vector, 1, 1),
 	                   centralMoment(vector, 2, 0) - centralMoment(vector, 0, 2));
 }
 
 // Feature 1: second moment about the central axis
-double mu22Alpha(std::vector<std::pair<int, int>>& vector, double alpha, double m00 = -1) {
+static double mu22Alpha(std::vector<std::pair<int, int>>& vector, double alpha, double m00 = -1) {
 	auto m = 0.0;
 	auto c = findCentroid(vector);
 	auto c_x = c.first;
@@ -57,7 +58,7 @@ double mu22Alpha(std::vector<std::pair<int, int>>& vector, double alpha, double 
 }
 
 // project points to the new co-ordinate system with the centroid as the origin
-void projectPoints(std::vector<std::pair<int, int>>& points, std::vector<std::pair<double, double>>& projected,
+static void projectPoints(std::vector<std::pair<int, int>>& points, std::vector<std::pair<double, double>>& projected,
                    double alpha, std::pair<int, int> centroid) {
 	auto c_x = centroid.first;
 	auto c_y = centroid.second;
@@ -71,13 +72,13 @@ void projectPoints(std::vector<std::pair<int, int>>& points, std::vector<std::pa
 
 // get the dims of bounding box [min_x, miny_y, max_x, max_y]
 template <typename T>
-T* boundingBoxDims(std::vector<std::pair<T, T>>& points) {
+static T* boundingBoxDims(std::vector<std::pair<T, T>>& points) {
 	T bb[4] = {0, 0, 0, 0};
 	for (const auto [x, y] : points) {
-		bb[0] = MIN(x, bb[0]);
-		bb[1] = MIN(y, bb[1]);
-		bb[2] = MAX(x, bb[2]);
-		bb[3] = MAX(y, bb[3]);
+		bb[0] = std::min(x, bb[0]);
+		bb[1] = std::min(y, bb[1]);
+		bb[2] = std::max(x, bb[2]);
+		bb[3] = std::max(y, bb[3]);
 	}
 	return bb;
 }
@@ -85,7 +86,7 @@ T* boundingBoxDims(std::vector<std::pair<T, T>>& points) {
 // one function which creates the bounding box
 
 // then the function which combines it all together and gets the feature
-void getFeatures(std::vector<std::pair<int, int>>& points, std::vector<double>& features) {
+static void getFeatures(std::vector<std::pair<int, int>>& points, std::vector<double>& features) {
 	auto alpha = findAlpha(points);
 	auto m00 = moment(points, 0, 0);
 	auto centroid = findCentroid(points, m00);
@@ -103,6 +104,6 @@ void getFeatures(std::vector<std::pair<int, int>>& points, std::vector<double>& 
 
 	// % of the box filled
 	features.push_back(m00 / (bb_h * bb_w));
-	
+
 	// TODO histogram of projection (16 buckets wide)
 }
